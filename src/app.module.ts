0fec890ac4d333e60e48893649c63db487bd8d37
sysnar <sysn4r@gmail.com>
2021-09-24 03:55:52 +0900

    Add auth module - user signup with hash encrypt

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { MovieModule } from './movie/movie.module';
 import { BoardsModule } from './boards/boards.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), MovieModule, BoardsModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), MovieModule, BoardsModule, AuthModule],
 })
 export class AppModule {}
